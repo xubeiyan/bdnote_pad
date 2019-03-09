99d767b9ceb193a73e6d1fadfdb8291a88029fa4
xubeiyan <yueyongdong@gmail.com>
2019-03-09 13:59:52 +0800

    build the basic structure...

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-const Koa = require('koa')
-const app = new Koa()
+const Koa = require('koa');
+const app = new Koa();
+const path = require('path');
 
 // 一些配置
-const config = require('./config/config.json')
+const config = require('./config/config.json');
 
-app.use( async ( ctx ) => {
-  ctx.body = 'hello koa2'
-})
+// 静态资源
+const static = require('koa-static');
+app.use(static(path.join(__dirname, config.staticPath)));
 
+// 模板引擎
+const render = require('koa-ejs');
+render(app, {
+	root: path.join(__dirname, config.ejs.root),
+	layout: config.ejs.layout,
+	viewExt: 'html',
+	cache: config.ejs.cache,
+	debug: config.ejs.debug,
+});
+
+// 路由
+const Router = require('koa-router');
+let router = new Router();
+
+	// 主页
+router.get('/', async (ctx, next) => {
+		await ctx.render('index', {
+			sub_title: 'Index'
+		});
+	})
+	// 查看某个乐谱
+	.get('/sheet/:id', async (ctx, next) => {
+		await ctx.render('sheet', {
+			sub_title: 'Sheet'
+		});
+	})
+	// 未找到
+	.get('/notfound', async (ctx, next) => {
+		await ctx.render('404', {
+			sub_title: 'Not Found'
+		});
+	});
+
+app.use(router.routes());
 app.listen(config.port)
+
 console.log(`[demo] start-quick is starting at port ${config.port}`)
