e2c3c9449a01a2c02c56b1613d9c7df4a99ec45c
yangyangdi <1@1.cn>
2023-04-10 14:55:25 +0800

    feature(frontend/src/App.jsx): add note.svg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Paper, Toolbar } from "@mui/material";
+import { Box, Paper, Toolbar } from "@mui/material";
 
 import { Routes, Route } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -33,7 +33,7 @@ function App() {
 
   // 底部菜单栏
   const [selected, setSelected] = useState(0);
-  
+
   // 页面跳转
   const nav = useNavigate();
   const toPage = (path, index) => {
@@ -42,20 +42,26 @@ function App() {
   }
 
   return (
-    <>
+    <Box sx={{ bgcolor: `#ddd` }}>
       <TopAppBar toggleMenu={toggleMenu} />
       <SideMenu open={menu.open} closeMenu={closeMenu} />
       <Toolbar />
-      <Routes>
-        <Route index path='/' element={<Main />} />
-        <Route index path='/search' element={<Search />} />
-        <Route index path='/user' element={<User />} />
-      </Routes>
+      <Paper sx={{
+        minHeight: 'calc(100vh - 48px - 56px - 1em)',
+        mx: `.5em`, mb: `.5em`, p: `.5em`,
+      }}
+        elevation={6}>
+        <Routes>
+          <Route index path='/' element={<Main />} />
+          <Route index path='/search' element={<Search />} />
+          <Route index path='/user' element={<User />} />
+        </Routes>
+      </Paper>
       <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
         elevation={3}>
-        <Navigation selected={selected} toPage={toPage}/>
+        <Navigation selected={selected} toPage={toPage} />
       </Paper>
-    </>
+    </Box>
   )
 }
 
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,8 +1,16 @@
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 function Search() {
   return (
-    <div>Search</div>
+    <Box>
+      <Stack alignItems="center">
+        <Typography variant='h4' sx={{ my: `2em` }}>Search Music Note...</Typography>
+        <TextField variant='standard' fullWidth
+          placeholder='Song Name, Author, Difficulty, ...' />
+        <Button sx={{ mt: `1em` }} variant='contained'>Start Search</Button>
+      </Stack>
+    </Box>
   )
 }
 
